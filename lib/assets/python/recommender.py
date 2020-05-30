91b7d8c1d41cb619245ec8ce2a01e7962d77f7d5
Louis Gatinet <louis.gatinet@gmail.com>
2020-05-30 12:17:07 +0200

    loeading ingredients csv

--- lib/assets/python/recommender.py
+++ lib/assets/python/recommender.py
@@ -3,8 +3,7 @@ import pandas as pd
 import numpy as np
 import collections
 # from gensim.models import word2vec
-from gensim.models import Word2Vec
-# from matplotlib import pyplot
+# from gensim.models import Word2Vec
 # from scipy import spatial
 
 arg_1 = sys.argv[1]
@@ -12,3 +11,7 @@ arg_2 = sys.argv[2]
 
 print("arg1:", arg_1)
 print("arg2:", arg_2)
+ingredients_df = pd.read_csv('/home/louis/code/gatinetlouis/KROK-Follow-Up/db/datas/ingredients.csv', sep=";")
+recipes_df = pd.read_csv('/home/louis/code/gatinetlouis/KROK-Follow-Up/db/datas/recipes.csv',sep=";")
+print(ingredients_df.shape)
+print(recipes_df.shape)
